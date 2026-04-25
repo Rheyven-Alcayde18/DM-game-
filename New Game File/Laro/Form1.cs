4a7a7fb662a132e47e8babed0270a09a918dac3b
Rhey <0325-1164@lspu.edu.ph>
2026-04-25 16:03:24 +0800

    New Forms Added

---
+++ New Game File/Laro/Form1.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CJ
+ * Date: 4/24/2026
+ * Time: 6:07 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Laro
+{
+	/// <summary>
+	/// Description of Form1.
+	/// </summary>
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void BckBtnClick(object sender, EventArgs e)
+		{
+			Form MainForm = new MainForm();
+			MainForm.Show();
+			this.Close();
+		}
+		void NewGameClick(object sender, EventArgs e)
+		{
+	
+		}
+		void ContinueClick(object sender, EventArgs e)
+		{
+	
+		}
+	}
+}
---
+++ New Game File/Laro/New Game.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CJ
+ * Date: 4/25/2026
+ * Time: 3:04 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DefaultNamespace
+{
+	/// <summary>
+	/// Description of Form2.
+	/// </summary>
+	public partial class Form2 : Form
+	{
+		public Form2()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
